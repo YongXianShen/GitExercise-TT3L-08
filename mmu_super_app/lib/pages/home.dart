300caa976ba39de57db04267d2246f43c1f4069e
Tan Park Zhong <tpzrocker@gmail.com>
2024-05-01 04:20:00 +0800

    homepage

--- mmu_super_app/lib/pages/home.dart
+++ mmu_super_app/lib/pages/home.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mmu_super_app/models/features_model.dart'; // Assuming this is the correct path
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<FeaturesModel> features = []; // Initialize with empty list
+
+  @override
+  void initState() {
+    super.initState();
+    _getFeatures(); // Call _getFeatures() in initState
+  }
+
+  void _getFeatures() async {
+    // Assuming getFeatures() is asynchronous, add async
+    features.addAll(await FeaturesModel.getFeatures()); // Update state with features
+    setState(() {}); // Notify UI of changes
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,8 +67,7 @@ class HomePage extends StatelessWidget {
                     ),
                     suffixIcon: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: SvgPicture.asset('assets/icons/Filter.svg'),
-                    ),
+                    ), // Remove unnecessary padding for suffixIcon
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
                       borderSide: BorderSide.none,
@@ -56,6 +75,53 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 40.0),
+                Container(
+                  height: 120,
+                  color: Colors.green,
+                  child: ListView.builder(
+                    itemCount: features.length, 
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.only(
+                      Left:20,
+                      right: 20
+                    ),
+                    seperatorBuilder: (context, index) => SizedBox(width: 25,),// Use features.length
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: features[index].boxColor.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(16)// Access boxColor property
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width:50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: SvgPicture.asset(features[index].iconPath),
+                              ),
+                            ),
+                           Text(
+                            features[index].name,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black,
+                              fontSize: 14
+                            ),
+                           )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ],
             ),
           ),

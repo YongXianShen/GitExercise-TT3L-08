e35781a696e896a83c69a02707f90f58b498c3c9
Tan Park Zhong <tpzrocker@gmail.com>
2024-04-30 19:06:24 +0800

    added search bar

---
+++ mmu_super_app/lib/pages/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'MMU Student App',
+          style: TextStyle(
+            color: Colors.orange,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.white ,
+        centerTitle: true,
+        toolbarHeight: 100.0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40,left: 20,right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff1D1617).withOpacity(0.15),
+                  blurRadius: 40,
+                  spreadRadius: 0.0
+                )
+              ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: EdgeInsets.all(15),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SvgPicture.asset('assets/icons/Search.svg'),
+                ),
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset('assets/icons/Filter.svg'),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none
+                )
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

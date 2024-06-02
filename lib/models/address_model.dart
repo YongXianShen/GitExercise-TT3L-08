aa952bb8a64e08f2213ef67ea3d5eeca8eb20bfe
Tan Park Zhong <tpzrocker@gmail.com>
2024-06-02 23:50:30 +0800

    added dropOff selection

--- lib/models/address_model.dart
+++ lib/models/address_model.dart
@@ -3,6 +3,8 @@ class AddressModel
   String? humanReadableAddress;
   double? latitudePosition;
   double? longitudePosition;
+  String? placeName;
+  String? placeID;
 
-  AddressModel({this.humanReadableAddress,this.latitudePosition, this.longitudePosition});
+  AddressModel({this.humanReadableAddress,this.latitudePosition, this.longitudePosition, this.placeID, this.placeName,});
 }

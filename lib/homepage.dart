4bfa5f2bf086b5da917f14ccc42f335997006c5e
Yong Xian Shen <samsonyong123456789@gmail.com>
2024-06-04 17:03:09 +0800

    samson

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -26,64 +26,7 @@ class HomePage extends StatelessWidget {
         centerTitle:true,
         
       ),
-      body: const NavigationExample(),
-      
-    );
-  }
-}
-
-class NavigationExample extends StatefulWidget{
-  const NavigationExample({super.key});
-
-  @override
-  State<NavigationExample> createState() => _NavigationExampleState();
-}
-
-Widget build(BuildContext context) {
-    return const MaterialApp(home: NavigationExample());
-}
-
-class _NavigationExampleState extends State<NavigationExample> {
-  int currentPageIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: currentPageIndex,
-        children: const [
-          FeaturesCards(),
-          
-          SettingsPage(),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentPageIndex,
-        onTap: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Homepage',
-          ),
-          
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-      ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const Chat()),);
-          },
-          child: const Icon(Icons.chat),
-          ),
-
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: const FeaturesCards(),
       
     );
   }

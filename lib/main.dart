b960e663e584707c2811603627d3f64875ec2870
Adam Harris Nyirop <adamharrisnyirop@gmail.com>
2024-06-13 21:22:05 +0800

    My commit (13/6/2024)

--- lib/main.dart
+++ lib/main.dart
@@ -46,26 +46,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => SwipeBloc()..add(
-            LoadUsers(users: User.users)
-          )
-        )
-      ],
-      child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
-          return ChangeNotifierProvider(
-            create: (context) => AppInfo(),
-            child: MaterialApp(
-              debugShowCheckedModeBanner: false,
-              theme:themeProvider.themeData,
-              home: const HomePage(),
-            ),
-          );
-        }
-      )
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return ChangeNotifierProvider(
+          create: (context) => AppInfo(),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme:themeProvider.themeData,
+            home: const HomePage(),
+          ),
+        );
+      }
     );  
   }
 }

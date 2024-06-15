a1ccfc66b993401d24af07420907c740d6e15a2d
Adam Harris Nyirop <adamharrisnyirop@gmail.com>
2024-06-15 22:00:00 +0800

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget {
             child: MaterialApp(
               debugShowCheckedModeBanner: false,
               theme:themeProvider.themeData,
-              home: const HomePage(),
+              home: const LoginScreen(),
             ),
           );
         }
